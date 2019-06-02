a5db9ac3d8a40a346aac49db8e706dd67876a524
arjun <arjunthestar007@gmail.com>
2019-06-01 22:03:48 -0700

    converted to androidx library

--- app/src/main/java/com/example/arjun/pocjetpack/MainActivity.kt
+++ app/src/main/java/com/example/arjun/pocjetpack/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.arjun.pocjetpack
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
